48af0af72bf3948c580331eaec5f5d0371dc3823
patmens <nanay5077@gmail.com>
2023-06-02 05:41:40 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * print_list -> A function that prints all the elements of a list_t list
+ * print_list - A function that prints all the elements of a list_t list
  * @h: header pointer
- * Return: returns number of nodes
+ * Return: returns number of nodes in the list
  */
 
 size_t print_list(const list_t *h)
