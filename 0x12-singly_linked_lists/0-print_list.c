ce046b05e33eea6be6a4e64e7c3b7bb0f73366e4
patmens <nanay5077@gmail.com>
2023-06-02 03:16:10 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,19 +12,18 @@ size_t print_list(const list_t *h)
 {
   size_t counter = 0;
    for (; h != NULL; h = h->next)
-{
+{ 
 	if (h->str == NULL)
 	{
 		printf("[0] (nil)\n");
 		counter++;
 	}
 	else
-	{
+           	{
 		printf("[%u] %s\n", h->len, h->str);
-		counter++;
-	}
+		counter++
+
+		}
 }
-
-
-
-}
+return(counter);
+ }
