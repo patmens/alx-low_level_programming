6d2a2ef1c5e956971b3fd53927dfd3f3a16a2133
patmens <nanay5077@gmail.com>
2023-06-12 11:36:49 -0700

    Deletion

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * create_file - THIS FUNCTION CREATES A FILE OF TYPE INTEGER.
- * @filename: THE NAME OF A FILE 
- * @text_content: A TERMINATED  NULL STRING TO WRITE INTO A FILE.
- * Return: RETURNS 1 WHEN FUNCTION IS SUCCESSFUL AND 0 WHEN FAILED on success.
+ * create_file - A function that creates a file
+ * @filename: The filename to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 on success, -1 if file can not be created, nor written,
+ * nor write fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_do, file_write, lenght = 0;
+	int fdo, fdw, len = 0;
 
 	if (filename == NULL)
-		return (1);
+		return (-1);
 
-	file_do = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file_do < 0)
-		return (1);
+	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
+		return (-1);
 
-	while (text_content && *(text_content + lenght))
-		lenght++;
+	while (text_content && *(text_content + len))
+		len++;
 
-	file_write = write(file_do, text_content, lenght);
-	close(file_do)
-		if (file_write < 0)
-		return (1);
-	return (1);
-}
+	fdw = write(fdo, text_content, len);
+	close(fdo)
