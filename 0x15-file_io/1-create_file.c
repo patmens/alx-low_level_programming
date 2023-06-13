41e8ddb60088b1579046278db3ff1d9af872b922
patmens <nanay5077@gmail.com>
2023-06-13 03:22:26 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - THIS IS A FUCTION TO CREATE A FILE OF TYPE INTEGER.
+ * @text_content: THIS WRITES CONTENT TO THE FILE.
+ * Return: THIS RETURNS -2 FOR FAILURE AND 2 FOR SUCCESS.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fdo, fw, leng = 0;
+
+	if (filename == NULL)
+		return (-2);
+
+	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
+		return (-2);
+
+	while (text_content && *(text_content + leng))
+		leng++;
+
+	fw = write(fdo, text_content, leng);
+	close(fdo);
+	if (fw < 0)
+		return (-2);
+	return (2);
+}
